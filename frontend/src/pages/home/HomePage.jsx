dd8f54fce751d70a7666254327f964ed0407755d
dc-hub0 <deepakchauhan7721@gmail.com>
2025-04-14 11:38:03 +0530

    AuthPage Done

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,6 +1,12 @@
+import AuthScreen from "./AuthScreen"
+import HomeScreen from "./HomeScreen"
 
 const HomePage = () => {
-  return <div className="hero-bg h-screen"> home </div>;
-}
+    const user = false;;
+
+  return <div>
+      {user ? <HomeScreen /> : <AuthScreen /> }
+    </div>
+  }
 
 export default HomePage
