e2f387efcb536bbbb54ad54a5b313882566f74c1
dc-hub0 <deepakchauhan7721@gmail.com>
2025-04-21 13:09:29 +0530

    Added Fully Functional homeScreen

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -2,19 +2,28 @@ import Navbar from '../../components/Navbar'
 import { Link } from 'react-router-dom'
 import { Info, Play } from 'lucide-react'
 import useGetTrendingContent from '../../hooks/useGetTrendingContent';
+import { ORIGINAL_IMG_BASE_URL } from '../../utils/constants'
 
 const HomeScreen = () => {
 
     const { trendingContent } = useGetTrendingContent();
     console.log("trendingContent", trendingContent);
 
+    
+	if (!trendingContent)
+		return (
+			<div className='h-screen text-white relative'>
+				<Navbar />
+				<div className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer' />
+			</div>
+		);
 
     return (
         <>
             <div className='relative h-screen text-white '>
                 <Navbar />
 
-                <img src="/extraction.jpg"
+                <img src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
                     alt="Hero img"
                     className='absolute top-0 left-0 w-full h-full object-cover -z-50'
                 />
@@ -26,23 +35,29 @@ const HomeScreen = () => {
                     <div className='bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10 ' />
 
                     <div className='max-w-2xl'>
-                        <h1 className='mt-4 text-6xl font-extrabold text-balance'>Extraction</h1>
+                        <h1 className='mt-4 text-6xl font-extrabold text-balance'>
+                            {trendingContent?.title || trendingContent?.name}
+                        </h1>
                         <p className='mt-2 text-lg'>
-                            2014  | 18+
+                            {trendingContent?.release_date?.split("-")[0] ||
+                                trendingContent?.first_air_date.split("-")[0]}{" "}
+                            | {trendingContent?.adult ? "18+" : "PG-13"}
                         </p>
                         <p className='mt-4 text-lg'>
-                            chris hemsworth, jessica chastain, and more star in this action-packed thriller about a black ops mercenary on a mission to rescue an Indian crime lord's kidnapped son.
+                        {trendingContent?.overview.length > 200
+								? trendingContent?.overview.slice(0, 200) + "..."
+								: trendingContent?.overview}
                         </p>
                     </div>
 
                     <div className='flex mt-8'>
-                        <Link to="/watch/123"
+                        <Link to={`/watch/${trendingContent?.id}`}
                             className='bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex'>
                             <Play className='size-6 mr-2 fill-black' />
                             Play
                         </Link>
 
-                        <Link to="/watch/123"
+                        <Link to={`/watch/${trendingContent?.id}`}
                             className='bg-gray-500 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center'>
                             <Info className='size-6 mr-2 ' />
                             More Info
@@ -53,6 +68,7 @@ const HomeScreen = () => {
 
                 </div>
             </div>
+            
         </>
     );
 };
